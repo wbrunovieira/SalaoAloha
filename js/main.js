bc063aebbed022fe978d34198f1eabec37ddaa2c
wbrunovieira <wbrunovieira77@gmail.com>
2023-11-29 18:36:17 -0300

    added lightbox modal gallety Fancybox4

--- js/main.js
+++ js/main.js
@@ -1,64 +1,49 @@
-let currentImageIndex = 0;
-
-const images = [
-    "images/work1.jpg",
-    "images/work2.jpg",
-    "images/work3.jpg",
-    "images/work4.jpg",
-    "images/work5.jpg",
-    "images/work6.jpg",
-    "images/work7.jpg",
-    "images/work8.jpg",
-    "images/work9.jpg",
-];
 
 
+document.querySelectorAll('[data-fancybox="gallery"]').forEach(function(element) {
+    Fancybox.bind(element, {
+      buttons: [
+        "zoom",
+        "share",
+        "slideShow",
+        "fullScreen",
+        "download",
+        "thumbs",
+        "close"
+      ],
+      loop: true,
+      speed: 300,
+      transitionEffect: "slide",
+      closeBtn: true,
+      keyboard: true,
+
+      mobile: {
+        preventCaptionOverlap: false,
+        idleTime: false,
+        clickContent: function(current, event) {
+          return current.type === "image" ? "toggleControls" : "close";
+        },
+        clickSlide: function(current, event) {
+          return current.type === "image" ? "toggleControls" : "close";
+        },
+        dblclickContent: false,
+        dblclickSlide: false
+      }
 
 
-    // loader
-    
-      
-
-
-    // our work
+    });
+  });
   
 
-document.addEventListener('DOMContentLoaded', (event) => {
-        document.querySelector('.close').addEventListener('click', closeLightbox);
-    });
+
+ 
+
+    
          
 
 
-document.querySelectorAll('.portfolio-popup').forEach(item => {
-    item.addEventListener('click', function(event) {
-        event.preventDefault();
-        openLightbox(this.href);
-    });
-});
-
-function openLightbox(src) {
-    currentImageIndex = images.indexOf(src);
-    if (currentImageIndex === -1) return; // URL não encontrado no array
-
-    document.getElementById('lightbox').style.display = 'block';
-    document.querySelector('.lightbox-content').src = src;
-}
 
 
-function closeLightbox() {
-    document.getElementById('lightbox').style.display = 'none';
-}
 
-function changeImage(direction) {
-    currentImageIndex += direction;
 
-    // Verifique os limites do array
-    if (currentImageIndex < 0) {
-        currentImageIndex = images.length - 1;
-    } else if (currentImageIndex >= images.length) {
-        currentImageIndex = 0;
-    }
-
-    document.querySelector('.lightbox-content').src = images[currentImageIndex];
-}
 
