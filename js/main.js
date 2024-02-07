e6c579c7ee1bc2366247ccefe2f97de256bfdc5a
wbrunovieira <wbrunovieira77@gmail.com>
2024-02-07 17:19:58 -0300

    nav elements one each tip

--- js/main.js
+++ js/main.js
@@ -1,103 +1,108 @@
-
-
-document.querySelectorAll('[data-fancybox="gallery"]').forEach(function(element) {
+document
+  .querySelectorAll('[data-fancybox="gallery"]')
+  .forEach(function (element) {
     Fancybox.bind(element, {
       buttons: [
-        "zoom",
-        "share",
-        "slideShow",
-        "fullScreen",
-        "download",
-        "thumbs",
-        "close"
+        'zoom',
+        'share',
+        'slideShow',
+        'fullScreen',
+        'download',
+        'thumbs',
+        'close',
       ],
       loop: true,
       speed: 500,
-      transitionEffect: "slide",
+      transitionEffect: 'slide',
       closeBtn: true,
       keyboard: true,
 
       mobile: {
         preventCaptionOverlap: false,
         idleTime: false,
-        clickContent: function(current, event) {
-          return current.type === "image" ? "toggleControls" : "close";
+        clickContent: function (current, event) {
+          return current.type === 'image' ? 'toggleControls' : 'close';
         },
-        clickSlide: function(current, event) {
-          return current.type === "image" ? "toggleControls" : "close";
+        clickSlide: function (current, event) {
+          return current.type === 'image' ? 'toggleControls' : 'close';
         },
         dblclickContent: false,
-        dblclickSlide: false
-      }
-
-
+        dblclickSlide: false,
+      },
     });
   });
 
-  document.addEventListener('scroll', function() {
-    const elements = document.querySelectorAll('.hidden, .animate__slideInLeft');
-    elements.forEach((el) => {
-      const rect = el.getBoundingClientRect();
-      const elemTop = rect.top;
-      const elemBottom = rect.bottom;
-  
-      // Verifica se o elemento está parcialmente visível
-      const isVisible = (elemTop < window.innerHeight) && (elemBottom >= 0);
-      if (isVisible) {
-        el.classList.remove('hidden');
-        el.classList.add('animate__slideInLeft');
-  
-        // Encontre e anime os elementos internos específicos
-        const innerAnimations = el.querySelectorAll('.animate__rotateInDownLeft');
-        innerAnimations.forEach(innerEl => {
-          innerEl.classList.add('animate__animated');
-        });
-  
-      } else {
-        el.classList.add('hidden');
-        el.classList.remove('animate__slideInLeft');
-  
-        // Resetar animações internas
-        const innerAnimations = el.querySelectorAll('.animate__rotateInDownLeft');
-        innerAnimations.forEach(innerEl => {
-          innerEl.classList.remove('animate__animated');
-        });
-      }
-    });
-  });
+document.addEventListener('scroll', function () {
+  const elements = document.querySelectorAll('.hidden, .animate__slideInLeft');
+  elements.forEach((el) => {
+    const rect = el.getBoundingClientRect();
+    const elemTop = rect.top;
+    const elemBottom = rect.bottom;
 
-  document.addEventListener('DOMContentLoaded', function() {
-    setInterval(function() {
-      var messageBox = document.querySelector('.whatsapp-message-box');
-      messageBox.classList.remove('animate__animated', 'animate__headShake', 'animate__shakeX');
-      
-      // Reforçar o reflow do navegador, necessário para reiniciar a animação
-      void messageBox.offsetWidth;
-      
-      messageBox.classList.add('animate__animated', 'animate__headShake', 'animate__shakeX');
-    }, 10000); 
-  });
-  
-  
-  var animacao = lottie.loadAnimation({
-    container: document.getElementById('calendario-animado'), 
-    renderer: 'svg', 
-    loop: true, 
-    autoplay: true, 
-    path: 'calendar.json' 
-  });
+    // Verifica se o elemento está parcialmente visível
+    const isVisible = elemTop < window.innerHeight && elemBottom >= 0;
+    if (isVisible) {
+      el.classList.remove('hidden');
+      el.classList.add('animate__slideInLeft');
 
+      // Encontre e anime os elementos internos específicos
+      const innerAnimations = el.querySelectorAll('.animate__rotateInDownLeft');
+      innerAnimations.forEach((innerEl) => {
+        innerEl.classList.add('animate__animated');
+      });
+    } else {
+      el.classList.add('hidden');
+      el.classList.remove('animate__slideInLeft');
 
-document.querySelector('.acceptButton').addEventListener('click', function() {
-    document.querySelector('.cookieCard').style.display = 'none';
+      // Resetar animações internas
+      const innerAnimations = el.querySelectorAll('.animate__rotateInDownLeft');
+      innerAnimations.forEach((innerEl) => {
+        innerEl.classList.remove('animate__animated');
+      });
+    }
+  });
 });
 
+document.addEventListener('DOMContentLoaded', function () {
+  setInterval(function () {
+    var messageBox = document.querySelector('.whatsapp-message-box');
+    messageBox.classList.remove(
+      'animate__animated',
+      'animate__headShake',
+      'animate__shakeX'
+    );
+
+    // Reforçar o reflow do navegador, necessário para reiniciar a animação
+    void messageBox.offsetWidth;
+
+    messageBox.classList.add(
+      'animate__animated',
+      'animate__headShake',
+      'animate__shakeX'
+    );
+  }, 10000);
+});
+
+var animacao = lottie.loadAnimation({
+  container: document.getElementById('calendario-animado'),
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: 'calendar.json',
+});
+
+// document.querySelector('.acceptButton').addEventListener('click', function () {
+//   document.querySelector('.cookieCard').style.display = 'none';
+// });
+
 document.addEventListener('DOMContentLoaded', (event) => {
-  document.querySelectorAll('.cardl').forEach(cardl => {
-    cardl.addEventListener('mousemove', e => {
+  document.querySelectorAll('.cardl').forEach((cardl) => {
+    cardl.addEventListener('mousemove', (e) => {
       const light = cardl.querySelector('.card-lightl');
       light.style.opacity = 1;
-      light.style.left = `${e.pageX - cardl.getBoundingClientRect().left - 75}px`;
+      light.style.left = `${
+        e.pageX - cardl.getBoundingClientRect().left - 75
+      }px`;
       light.style.top = `${e.pageY - cardl.getBoundingClientRect().top - 75}px`;
     });
 
@@ -108,13 +113,16 @@ document.addEventListener('DOMContentLoaded', (event) => {
   });
 });
 
-
-
-         
-
-
-
-
-
-
-
+function toggleNavbar() {
+  const navbarSupportedContent = document.getElementById(
+    'navbarSupportedContent'
+  );
+  if (navbarSupportedContent.classList.contains('show')) {
+    navbarSupportedContent.classList.remove('show');
+  } else {
+    navbarSupportedContent.classList.add('show');
+  }
+}
+// document.querySelector('.show').addEventListener('click', function () {
+//   this.classList.toggle('active');
+// });
